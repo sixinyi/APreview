44d2524224563a1b123371c6f414816abce15a29
yangwenxuan <yangwenxuans@outlook.com>
2020-04-14 21:04:26 -0400

    new pc

---
+++ src/Hailstone.java
@@ -0,0 +1,33 @@
+public class Hailstone {
+
+    /** Returns the length of a hailstone sequence that starts with n,
+     * as described in part (a).
+     * Precondition: n > 0
+     */
+    public static int hailstoneLength(int n) {
+        /* to be implemented in part (a) */
+
+    }
+
+
+    /** Returns true if the hailstone sequence that starts with n is considered long
+     * and false otherwise, as described in part (b).
+     * Precondition: n > 0
+     */
+    public static boolean isLongSeq(int n) {
+        /* to be implemented in part (b) */
+
+    }
+
+
+    /** Returns the proportion of the first n hailstone sequences that are considered long,
+     * as described in part (c).
+     * Precondition: n > 0
+     */
+    public static double propLong(int n) {
+        /* to be implemented in part (c) */
+
+    }
+
+    // There may be instance variables, constructors, and methods not shown.
+}
