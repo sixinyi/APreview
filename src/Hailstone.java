54c27b34c12bf94b47e22cae0c6dc12a94c8a8b1
yangwenxuan <yangwenxuans@outlook.com>
2020-04-25 09:49:04 -0400

    hailstone finish

--- src/Hailstone.java
+++ src/Hailstone.java
@@ -5,8 +5,29 @@ public class Hailstone {
      * Precondition: n > 0
      */
     public static int hailstoneLength(int n) {
-        /* to be implemented in part (a) */
+        /* to be implemented in part (a)
+        * rules: even = /2, odd = x3 +1
+        */
 
+        if (n == 1) {
+            return 1;
+
+        } else {
+            int num = 1;
+
+            while (n > 1) {
+
+                if (n % 2 != 0) {
+                    n = 3*n+1;
+                    num++;
+
+                } else {
+                    n /= 2;
+                    num++;
+                }
+            }
+            return num;
+        }
     }
 
 
@@ -14,9 +35,14 @@ public class Hailstone {
      * and false otherwise, as described in part (b).
      * Precondition: n > 0
      */
-    public static boolean isLongSeq(int n) {
+    public static boolean isLongSeq(int n){
         /* to be implemented in part (b) */
+        boolean res = false;
 
+        if (n < hailstoneLength(n)) {
+            res = true;
+        }
+        return res;
     }
 
 
@@ -24,10 +50,18 @@ public class Hailstone {
      * as described in part (c).
      * Precondition: n > 0
      */
-    public static double propLong(int n) {
+    public static double propLong(int n){
         /* to be implemented in part (c) */
+        double num = 0;
 
+        for (int i = 1; i <= n; i++) {
+            if (isLongSeq(i)) {
+                num++;
+            }
+        }
+        return num / n;
     }
 
     // There may be instance variables, constructors, and methods not shown.
+
 }
